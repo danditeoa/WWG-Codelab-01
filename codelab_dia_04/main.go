91d21cea6a807233f322e5c3d49e55141e1553c5
danditeoa <taci.dandi@gmail.com>
2019-10-17 17:02:16 -0300

    add(exercicios-dia4): create solutions for content array and slices

--- codelab_dia_04/main.go
+++ codelab_dia_04/main.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("coloque suas resoluções nesse arquivo :D")
+	array := [10]int{}
+	array[0] = 1
+	array[1] = 2
+	array[2] = 3
+	array[3] = 4
+	array[4] = 5
+	array[5] = 6
+	array[6] = 7
+	array[7] = 8
+	array[8] = 9
+	array[9] = 10
+
+	fmt.Println(array)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(array[i])
+	}
+
+	values := []int{111, 1, 8, 90, 21}
+
+	fmt.Print("Soma = ", soma(values))
+}
+
+	func soma(values []int) int {
+		soma := 0
+		for i := 0; i < len(values); i++ {
+			soma += values[i]
+		}
+	return soma
 }
